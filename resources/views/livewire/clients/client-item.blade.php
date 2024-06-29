f95e9429c09b66c02c9b82b4cd9047ccbd5bb013
speedwasswa <speedwasswa02@gmail.com>
2024-06-30 00:00:20 +0300

    client-item deleted

--- resources/views/livewire/clients/client-item.blade.php
+++
@@ -1,8 +0,0 @@
-<li onclick="window.location='{{route('client.show', $client)}}'" class="list-group-item d-flex justify-content-between align-items-center">
-    <span>{{$client->name}}<br>
-    <span class="small slate-light">{{$client->motor->registration}}</span></span>
-    <div>
-    <a href="{{route('luganda.document', $client)}}" class="btn btn-sm btn-light slate-light">Luganda</a>
-    <a href="{{route('english.document', $client)}}" class="btn btn-sm btn-light slate-light">English</a>
-    </div>
-</li> 

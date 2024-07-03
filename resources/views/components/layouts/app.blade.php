69de0b9deae2b411f471d312d9342dd14afd02e2
speedwasswa <speedwasswa02@gmail.com>
2024-07-03 13:12:10 +0300

    Finished the development phase

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -18,7 +18,9 @@
 </svg>
 
 <header class="navbar sticky-top flex-md-nowrap p-0 shadow bg-white">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 slate" href="#">kayondobodaloans</a>
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 slate" href="#">
+    kayondo boda loans
+</a>
   <ul class="navbar-nav flex-row d-md-none">
     <li class="nav-item text-nowrap">
       <button class="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
